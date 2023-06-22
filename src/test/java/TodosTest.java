92ad94cfbd2f70b7fcd63140843bebd75e2f7578
“Larisa” <lsergeevna1978@mail.ru>
2023-06-22 11:39:26 +0400

    Tests for sear.ch added

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -5,6 +5,103 @@ import org.junit.jupiter.api.Test;
 public class TodosTest {
 
     @Test
+    public void shouldSearchOneTask() {
+
+        SimpleTask simpleTask = new SimpleTask(7, "Дача встреча");
+        SimpleTask simpleTask2 = new SimpleTask(8, "Дача ремонт");
+        SimpleTask simpleTask3 = new SimpleTask(9, "Дача яблоки");
+
+        String[] subtasks = {"Дача окна", "Командировка", "Родители"};
+        Epic epic = new Epic(42, subtasks);
+
+        Meeting meeting = new Meeting(
+                323,
+                "Дача друзья",
+                "Встреча с друзьями",
+                "В воскресенье после обеда");
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
+        todos.add(epic);
+        todos.add(meeting);
+
+        //todos.findAll();
+
+        Task[] expected = {epic};
+        Task[] actual = todos.search("Дача окна");
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void shouldSearchSeveralTasks() {
+
+        SimpleTask simpleTask = new SimpleTask(7, "Дача встреча");
+        SimpleTask simpleTask2 = new SimpleTask(8, "Дача ремонт");
+        SimpleTask simpleTask3 = new SimpleTask(9, "Дача яблоки");
+
+        String[] subtasks = {"Дача окна", "Командировка", "Родители"};
+        Epic epic = new Epic(42, subtasks);
+
+        Meeting meeting = new Meeting(
+                323,
+                "Дача друзья",
+                "Встреча с друзьями",
+                "В воскресенье после обеда");
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
+        todos.add(epic);
+        todos.add(meeting);
+
+        //todos.findAll();
+
+        Task[] expected = {simpleTask, simpleTask2, simpleTask3, epic, meeting};
+        Task[] actual = todos.search("Дача");
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldNotSearchTasks() {
+
+        SimpleTask simpleTask = new SimpleTask(7, "Дача встреча");
+        SimpleTask simpleTask2 = new SimpleTask(8, "Дача ремонт");
+        SimpleTask simpleTask3 = new SimpleTask(9, "Дача яблоки");
+
+        String[] subtasks = {"Дача окна", "Командировка", "Родители"};
+        Epic epic = new Epic(42, subtasks);
+
+        Meeting meeting = new Meeting(
+                323,
+                "Дача друзья",
+                "Встреча с друзьями",
+                "В воскресенье после обеда");
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
+        todos.add(epic);
+        todos.add(meeting);
+
+        //todos.findAll();
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Дача с друзьями");
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
     public void shouldAddThreeTasksOfDifferentType() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родимтелям");
 
@@ -36,7 +133,6 @@ public class TodosTest {
         Task[] actual = todos.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
-
 
 
     @Test
@@ -120,6 +216,7 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
 
     }
+
     @Test
     public void shouldNotFindQueryIfTaskIsNotExist() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
